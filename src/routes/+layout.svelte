6b8dbb8221f3ccff79afef0073f879384c32a03d
Caner Akdas <canerakdas@gmail.com>
2023-05-08 20:58:19 +0300

    chore: color and typography

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,21 @@
+<script>
+	import { setContext } from 'svelte';
+	import { writable } from 'svelte/store';
+	import { RadioStatic } from 'radio-color-system';
+
+	import '$utils/_variables.scss';
+
+	let colors = writable([
+		{
+			color: { h: 5, s: 32, l: 48 },
+			name: ['primary', 'secondary', 'tertiary']
+		},
+		{ color: { h: 270, s: 2, l: 57 }, name: 'neutral' }
+	]);
+
+	setContext('colors', colors);
+</script>
+
+<RadioStatic colors={$colors} async={true} />
+
+<slot />
